e45f5e20f96c3162cccc9b4049754ada154f348c
Tony9442 <112698587+Tony9442@users.noreply.github.com>
2024-04-23 18:30:14 +0800

    new-change

--- Part1/courseinfo/src/App.jsx
+++ Part1/courseinfo/src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const Part = (props) => {
+  // const part1 = "Fundamentals of React";
+  // const exercises1 = 10;
+  // const part2 = "Using props to pass data";
+  // const exercises2 = 7;
+  // const part3 = "State of a component";
+  // const exercises3 = 14;
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p>
+        The part topic is {props.part} and the exercise is {props.exercises}
       </p>
+     
     </>
-  )
+  );
 }
 
-export default App
+const HeaderPage = (props) => {
+  const course = "Half Stack application development";
+  return (
+    <div>
+      <h1>{course}</h1>
+    </div>
+  );
+};
+const ContentPage = () => {
+  return (
+    <div>
+      <Part part="Fundamentals of  React part1" exercises={10} />
+      <Part part="Using props to pass data part2" exercises={7} />
+      <Part part="State of a component  part3" exercises={14} />
+    </div>
+  );
+};
+const TotalPage = () => {
+  const exercises1 = 10;
+  const exercises2 = 7;
+  const exercises3 = 14;
+  return (
+    <div>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    </div>
+  );
+};
+ 
+const App = () => {
+  return (
+    <div>
+      <HeaderPage />
+      <ContentPage />
+      <TotalPage />
+    </div>
+  );
+};
+
+export default App;
